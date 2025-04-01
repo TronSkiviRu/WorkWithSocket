94f840b52dd2383058a48f99cf2cc003fcda92aa
Ваше Имя <_email@example.com>
2025-04-01 22:51:43 +0700

    update Client.py

--- Ex2/Client.py
+++ Ex2/Client.py
@@ -1,5 +1,5 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('localhost', 10100))
-s.send(b'SAKURA')
+s.send('Теперь можно по русски:) а не байтовой строкой'.encode('utf-8'))
 s.close()
